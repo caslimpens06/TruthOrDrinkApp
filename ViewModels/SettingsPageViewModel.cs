41287fdd78bfd730ded9ea3e9cb408f5ffd1d855
Cas Limpens <2400410limpens@zuyd.nl>
2024-12-22 20:31:06 +0100

    settingspage

--- ViewModels/SettingsPageViewModel.cs
+++ ViewModels/SettingsPageViewModel.cs
@@ -1,42 +1,70 @@
 ﻿using System.Windows.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
 
-namespace TruthOrDrink.ViewModels
+namespace TruthOrDrink.ViewModels;
+
+public partial class SettingsPageViewModel : ObservableObject
 {
-	public partial class SettingsPageViewModel : ObservableObject
+	private int _playerNumber;
+	private bool _playerNumberReadOnly = true;
+	private string _toggleButtonText = "Bewerk";
+	private string _exampleSetting = "Default Setting Value";
+
+	public int PlayerNumber
 	{
-		[ObservableProperty]
-		private string _exampleSetting = "Default Setting Value";
+		get => _playerNumber;
+		set => SetProperty(ref _playerNumber, value);
+	}
 
+	public bool PlayerNumberReadOnly
+	{
+		get => _playerNumberReadOnly;
+		set => SetProperty(ref _playerNumberReadOnly, value);
+	}
 
-		public ICommand RefreshDrinkDataCommand { get; }
-		public ICommand OpenInstagramCommand { get; }
-		public ICommand OpenFacebookCommand { get; }
-		public ICommand OpenLinkedInCommand { get; }
+	public string ToggleButtonText
+	{
+		get => _toggleButtonText;
+		set => SetProperty(ref _toggleButtonText, value);
+	}
 
-		public SettingsPageViewModel()
+	public string ExampleSetting
+	{
+		get => _exampleSetting;
+		set => SetProperty(ref _exampleSetting, value);
+	}
+
+	public ICommand RefreshDrinkDataCommand { get; }
+	public ICommand OpenInstagramCommand { get; }
+	public ICommand OpenFacebookCommand { get; }
+	public ICommand OpenLinkedInCommand { get; }
+	public ICommand TogglePlayerCommand { get; }
+
+	public SettingsPageViewModel()
+	{
+		RefreshDrinkDataCommand = new Command(RefreshDrinkData);
+		OpenInstagramCommand = new Command(async () => await OpenUrlAsync("https://www.instagram.com/cas_limpens"));
+		OpenFacebookCommand = new Command(async () => await OpenUrlAsync("https://www.facebook.com/profile.php?id=100076414400998"));
+		OpenLinkedInCommand = new Command(async () => await OpenUrlAsync("https://www.linkedin.com/in/cas-limpens-a091742b1/"));
+		TogglePlayerCommand = new Command(TogglePlayerNumberReadOnly);
+	}
+
+	private void RefreshDrinkData()
+	{
+		// Voeg hier je logica toe om de drankgegevens te verversen.
+	}
+
+	private async Task OpenUrlAsync(string url)
+	{
+		if (Uri.TryCreate(url, UriKind.Absolute, out var uriResult))
 		{
-			RefreshDrinkDataCommand = new Command(RefreshDrinkData);
-			OpenInstagramCommand = new Command(OpenInstagram);
-			OpenFacebookCommand = new Command(OpenFacebook);
-			OpenLinkedInCommand = new Command(OpenLinkedIn);
+			await Launcher.OpenAsync(uriResult);
 		}
+	}
 
-		private void RefreshDrinkData() { }
-
-		private void OpenInstagram()
-		{
-			Launcher.OpenAsync("https://www.instagram.com/cas_limpens");
-		}
-
-		private void OpenFacebook()
-		{
-			Launcher.OpenAsync("https://www.facebook.com/profile.php?id=100076414400998");
-		}
-
-		private void OpenLinkedIn()
-		{
-			Launcher.OpenAsync("https://www.linkedin.com/in/cas-limpens-a091742b1/");
-		}
+	private void TogglePlayerNumberReadOnly()
+	{
+		_playerNumberReadOnly = !_playerNumberReadOnly;
+		_toggleButtonText = _playerNumberReadOnly ? "Bewerk" : "Klaar";
 	}
 }
